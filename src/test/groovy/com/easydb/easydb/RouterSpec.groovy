26171e94d178b668d723d817d7bd10273d8ec3bd
Bartlomiej Beczkowski <beczkowb@gmail.com>
2017-12-10 12:06:06 +0100

    tests skeleton

---
+++ src/test/groovy/com/easydb/easydb/RouterSpec.groovy
@@ -0,0 +1,11 @@
+package com.easydb.easydb
+
+class RouterSpec {
+    def "should dispatch query to proper space" () {
+
+    }
+
+    def "should dispatch operation to proper space" () {
+
+    }
+}
---
+++ src/test/groovy/com/easydb/easydb/SpaceSpec.groovy
@@ -0,0 +1,59 @@
+package com.easydb.easydb
+
+class SpaceSpec {
+    // one instance can contain whole application(multiple spaces)
+    // all space buckets should share single db
+    // all spaces with same definition should be connected to same db
+
+    // bucket > collection
+    // space > db
+    // instance == N spaces
+
+    def "should synchronize all spaces with same definition after operation is done" () {
+
+    }
+
+    def "should synchronize all buckets with same definition after operation is done" () {
+
+    }
+
+    def "should be empty after creation" () {
+
+    }
+
+    def "should add bucket" () {
+
+    }
+
+    def "should remove bucket" () {
+
+    }
+
+    def "should dispatch query to proper bucket" () {
+
+    }
+
+    def "should return query result" () {
+
+    }
+
+    def "should dispatch operation to proper bucket" () {
+
+    }
+
+    def "should return operation result" () {
+
+    }
+
+    def "should add subscriber" () {
+
+    }
+
+    def "should remove subscriber" () {
+
+    }
+
+    def "should send feed to subscribers after operation is done" () {
+
+    }
+}
