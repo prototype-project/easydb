be1236e668c8f8a6d82610155d7c0d456afe4941
Bartlomiej Beczkowski <beczkowb@gmail.com>
2017-12-12 18:08:03 +0100

    tests skeleton

--- src/test/groovy/com/easydb/easydb/SpaceSpec.groovy
+++ src/test/groovy/com/easydb/easydb/SpaceSpec.groovy
@@ -1,59 +1,35 @@
 package com.easydb.easydb
 
 class SpaceSpec {
-    // one instance can contain whole application(multiple spaces)
-    // all space buckets should share single db
-    // all spaces with same definition should be connected to same db
-
-    // bucket > collection
-    // space > db
-    // instance == N spaces
-
-    def "should synchronize all spaces with same definition after operation is done" () {
+    def "should create bucket"() {
 
     }
 
-    def "should synchronize all buckets with same definition after operation is done" () {
+    def "should remove bucket"() {
 
     }
 
-    def "should be empty after creation" () {
+    def "should throw error when trying to create bucket with non unique name"() {
 
     }
 
-    def "should add bucket" () {
+    def "should throw error when trying to remove nonexistent bucket"() {
 
     }
 
-    def "should remove bucket" () {
+    def "should add element to bucket"() {
 
     }
 
-    def "should dispatch query to proper bucket" () {
+    def "should remove element from bucket"() {
 
     }
 
-    def "should return query result" () {
+    def "should throw error when trying to remove nonexistent element"() {
 
     }
 
-    def "should dispatch operation to proper bucket" () {
-
-    }
-
-    def "should return operation result" () {
-
-    }
-
-    def "should add subscriber" () {
-
-    }
-
-    def "should remove subscriber" () {
-
-    }
-
-    def "should send feed to subscribers after operation is done" () {
+    def ""() {
 
     }
 }
