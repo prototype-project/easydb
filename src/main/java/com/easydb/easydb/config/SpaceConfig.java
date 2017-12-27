fd7df17113d94d0198a4c45c27962ae727830dad
Bartlomiej Beczkowski <beczkowb@gmail.com>
2017-12-27 19:55:36 +0100

    adding space.name param

--- src/main/java/com/easydb/easydb/config/SpaceConfig.java
+++ src/main/java/com/easydb/easydb/config/SpaceConfig.java
@@ -3,6 +3,8 @@ package com.easydb.easydb.config;
 import com.easydb.easydb.domain.bucket.BucketRepository;
 import com.easydb.easydb.domain.MainSpace;
 import com.easydb.easydb.infrastructure.MongoBucketRepository;
+import com.mongodb.Mongo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -15,7 +17,7 @@ public class SpaceConfig {
     }
 
     @Bean
-    MongoBucketRepository bucketRepository(MongoTemplate mongoTemplate) {
-        return new MongoBucketRepository(mongoTemplate);
+    MongoBucketRepository bucketRepository(Mongo mongo, @Value("${space.name}") String spaceName) {
+        return new MongoBucketRepository(new MongoTemplate(mongo, spaceName));
     }
 }
