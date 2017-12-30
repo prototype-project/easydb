44468bab6aa0c3dc3ee3ea2ad9b66a646e59bb0b
Bartlomiej Beczkowski <beczkowb@gmail.com>
2017-12-30 23:36:13 +0100

    changing config

--- src/integration/groovy/com/easydb/easydb/space/SpaceTestConfig.groovy
+++ src/integration/groovy/com/easydb/easydb/space/SpaceTestConfig.groovy
@@ -4,22 +4,43 @@ import com.easydb.easydb.domain.MainSpace
 import com.easydb.easydb.domain.UUIDProvider
 import com.easydb.easydb.infrastructure.bucket.MongoBucketRepository
 import com.github.fakemongo.Fongo
+import com.mongodb.Mongo
+import com.mongodb.MongoClient
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
+import org.springframework.data.mongodb.MongoDbFactory
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory
 
+@Configuration
 class SpaceTestConfig {
     private static final String SPACE_NAME = "testSpace";
     private static final String SERVER_NAME = "testServer";
 
     @Bean
+    Mongo mongo() {
+        Fongo fongo = new Fongo(SERVER_NAME)
+        return fongo.getMongo()
+    }
+
     @Primary
-    static MainSpace space() {
-        Fongo fongo = new Fongo(SERVER_NAME);
+    @Bean
+    MainSpace space(Mongo mongo) {
         MongoBucketRepository bucketRepository = new MongoBucketRepository(
-                new MongoTemplate(fongo.getMongo(), SPACE_NAME));
+                new MongoTemplate(mongo, SPACE_NAME))
         UUIDProvider uuidProvider = new UUIDProvider();
 
         return new MainSpace(bucketRepository, uuidProvider);
+    }
+
+    @Bean
+    MongoClient mongoClient() {
+        return new MongoClient("mongodb://localhost:27017/" + SPACE_NAME)
+    }
+
+    @Bean
+    MongoDbFactory mongoDbFactory(Mongo mongo) {
+        return new SimpleMongoDbFactory(mongo, SPACE_NAME)
     }
 }
