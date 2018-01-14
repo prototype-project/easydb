bd676cbba1384f7a7c8aea4a93653538695bd602
Bartlomiej Beczkowski <beczkowb@gmail.com>
2018-01-14 11:53:58 +0100

    removing ending / from path

--- src/main/java/com/easydb/easydb/api/SpaceDefinitionController.java
+++ src/main/java/com/easydb/easydb/api/SpaceDefinitionController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/v1/spaces/")
+@RequestMapping(value = "/api/v1/spaces")
 class SpaceDefinitionController {
 
 	private final SpaceDefinitionRepository spaceDefinitionRepository;
