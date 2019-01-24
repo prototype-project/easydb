2c41cc2adb3e90929ce365db9bdbc114153a73df
Daniel Faderski <daniel.faderski@allegro.pl>
2019-01-25 00:22:46 +0100

    Dockerize easydb

--- vagrant-deploy/files/mongoRouterInit.js
+++
@@ -1,3 +0,0 @@
-sh.addShard("10.10.10.11:27017");
-sh.addShard("10.10.10.12:27017");
-sh.enableSharding("easydb");
--- vagrant-deploy/scripts/init_slave.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export HOME="/home/vagrant"
-
-source $HOME/install.sh
-source $HOME/provision_supervisor.sh
-rm $HOME/install.sh
-rm $HOME/provision_supervisor.sh
