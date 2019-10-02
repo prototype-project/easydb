cc5fa7fa1064665d4dbc59fbdba4cfd3698045ab
Daniel Faderski <daniel.faderski@allegro.pl>
2019-10-03 00:15:17 +0200

    Fix testcontainer

--- .docker/files/testcontainer_starter.sh
+++ .docker/files/testcontainer_starter.sh
@@ -2,7 +2,10 @@
 
 mongod &
 mongo localhost:27017/easydb < /opt/easydb/resources/init_dev_mongo.js
-
+while [ $? -eq 1 ]
+do
+    mongo localhost:27017/easydb < /opt/easydb/resources/init_dev_mongo.js
+done
 /bin/bash /opt/zookeeper/bin/zkServer.sh start /opt/zookeeper/conf/zoo_sample.cfg
 
-/opt/easydb/dist/bin/easydb -port 9000 -environment local;
+/opt/easydb/dist/bin/easydb -port 9000 -environment local
